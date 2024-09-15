231971e02761301e1fc8375573b65091ea412793
HoangHuong29022004 <phamhoanghuong04@gmail.com>
2024-09-15 20:40:24 +0700

    save

---
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       $user = new User();
+       $user->name = "admin";
+       $user->email = "admin@email.com";
+       $user->password = Hash::make("123123");
+       $user->save();
+    }
+}
