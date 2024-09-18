f2022a35b9a24c8a8559b841636cfba5ef75d899
HoangHuong29022004 <phamhoanghuong04@gmail.com>
2024-09-18 22:59:17 +0700

    save

--- Backend/app/Providers/AppServiceProvider.php
+++ Backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Modules\Attributes\Repositories\AttributeRepository;
+use Modules\Attributes\Repositories\AttributeRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->bindingsRepository();
+    }
+
+    public function bindingsRepository()
+    {
+        // Attribute
+        $this->app->singleton(
+            AttributeRepositoryInterface::class,
+            AttributeRepository::class
+        );
     }
 
     /**
